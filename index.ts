9bb1083a846f6c6e7feaaeaef0d80fb2428293e1
Hassan0026 <159889462+Hassan0026@users.noreply.github.com>
2024-03-24 12:25:50 +0500

    my first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+let Name: string = "\t\n Hassan \t\n";
+console.log(Name);
+
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 *2);
+console.log(16 / 2);
+
+
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(2 * 4);
+console.log(16 / 2);
